0213c4ebcd516b225b3ef76a2828ad2ef4ba1230
Eduardo <almazan94@gmail.com>
2025-03-10 16:26:32 +0100

    test baselines

--- src/validation.py
+++ src/validation.py
@@ -264,7 +264,7 @@ def format_comparison_results(comparison_results):
 
     return df_metrics
 
-def validate_forecast(naive_df, forecast_df, baseline_df, to_forecast_column, forecasted_column):
+def validate_forecast(naive_df, forecast_df, baseline_df, to_forecast_column, forecasted_column, baseline_column = 'naive_forecast'):
     plot_time_series_forecast(forecast_df, [to_forecast_column, forecasted_column], 0.9, '--', True, 'sqrt')
 
     model_metrics = calculate_forecast_metrics(naive_df, forecast_df, to_forecast_column, forecasted_column)
@@ -277,18 +277,18 @@ def validate_forecast(naive_df, forecast_df, baseline_df, to_forecast_column, fo
 
     check_forecast_residuals(forecast_df, to_forecast_column, forecasted_column)
 
-    comparison_results = compare_forecast_models(naive_df, baseline_df, forecast_df, to_forecast_column, forecasted_column)
+    comparison_results = compare_forecast_models(naive_df, baseline_df, forecast_df, to_forecast_column, forecasted_column, baseline_column = baseline_column)
     formatted_results_df = format_comparison_results(comparison_results)
     display(formatted_results_df)
     return model_metrics, comparison_results
 
-def test_forecast(naive_df, forecast_df, baseline_df, to_forecast_column, forecasted_column, n_test = 60):
+def test_forecast(naive_df, forecast_df, baseline_df, to_forecast_column, forecasted_column, n_test = 60, baseline_column = 'naive_forecast'):
     print_title('TEST TEMPORAL BACKTESTING')
     plot_time_series_forecast(forecast_df, [to_forecast_column, forecasted_column], 0.9, '--', True, 'sqrt')
 
     model_metrics = calculate_forecast_metrics(naive_df, forecast_df, to_forecast_column, forecasted_column)
 
-    comparison_results = compare_forecast_models(naive_df[:-n_test], baseline_df[:-n_test], forecast_df[:-n_test], to_forecast_column, forecasted_column)
+    comparison_results = compare_forecast_models(naive_df[:-n_test], baseline_df[:-n_test], forecast_df[:-n_test], to_forecast_column, forecasted_column , baseline_column = baseline_column)
     formatted_results_df = format_comparison_results(comparison_results)
     display(formatted_results_df)
     return model_metrics, comparison_results
